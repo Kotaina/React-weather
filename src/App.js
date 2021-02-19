ec68bc2ce3239574c29e34091a82d50a87f12ef8
Kotaina <kotaina217+git@gmail.com>
2021-02-19 12:40:27 +0300

    upds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Info from "./components/info"
+import Form from "./components/form"
+import Weather from "./components/weather"
+
+const API_KEY = "4a96273f9c1eeedc2f7a5f7465f4b263";
 
 class App extends React.Component {
+
+    gettingWeather = async () => {
+        // const api_url = await fetch('http://api.openweathermap.org/data/2.5/weather?q={Moscow}&appid={${API_KEY}}'); Моё, но пока не активно
+        const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric)`);
+        const data = await api_url.json();
+        console.log(data);
+    }
+
     render() {
         return (
             <div>
                 <Info />
+                <Form />
+                <Weather />
             </div>
         )
     }
---
+++ src/components/form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>Новая форма</div>
+        )
+    }
+}
+
+export default Form;
